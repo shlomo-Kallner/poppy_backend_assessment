94b6cbed830725c981146b70f9d80c7c706c2961
Shlomo Kallner <shlomo.kallner.omputers@gmail.com>
2023-09-15 17:56:02 +0300

    Initial Additiona of additional Plugins - WIP..

--- src/poppy_s/lib/schemas/plugins.py
+++ src/poppy_s/lib/schemas/plugins.py
@@ -1,7 +1,25 @@
 #!/bin/env python3
 
-from typing import List, Optional, Type, TypeVar
+from typing import List, Optional, Type, TypeVar, Callable, Sequence, Union, Any
 
 from fastapi import APIRouter, Depends, HTTPException, Query
+from fastapi.responses import JSONResponse
 
+from fastapi.routing import APIRoute, generate_unique_id, Default
+from starlette.routing import BaseRoute
 from pydantic import BaseModel
+
+
+class RouterPluginSchema(BaseModel):
+    router: APIRouter | Callable[...,APIRouter]
+    prefix: str = ""
+    tags: Optional[List[Union[str, Enum]]] = None
+    dependencies: Optional[Sequence[Depends]] = None
+    default_response_class: Type[Response] = Default(JSONResponse)
+    responses: Optional[Dict[Union[int, str], Dict[str, Any]]] = None
+    callbacks: Optional[List[BaseRoute]] = None
+    deprecated: Optional[bool] = None
+    include_in_schema: bool = True
+    generate_unique_id_function: Callable[[APIRoute], str] = Default(
+        generate_unique_id
+    )
